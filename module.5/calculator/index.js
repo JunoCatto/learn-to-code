3481b9e877875439c74c73c878e2e0f0d41c7e59
Juno <owen.g.howells@gmail.com>
2025-11-07 10:58:10 +1100

    asd

--- module.5/calculator/index.js
+++ module.5/calculator/index.js
@@ -1,5 +1,7 @@
 // create express app to basic math functions
 import express from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 const app = express();
 const port = 3000;
 
@@ -8,6 +10,24 @@ app.get("/add", (req, res) => {
   const a = Number(req.query.a);
   const b = Number(req.query.b);
   res.send(`${a} + ${b} = ${a + b}`);
+});
+
+app.get("/sub", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.send(`${a} - ${b} = ${a - b}`);
+});
+
+app.get("/mul", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.send(`${a} * ${b} = ${a * b}`);
+});
+
+app.get("/div", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.send(`${a} / ${b} = ${a / b}`);
 });
 
 app.listen(port, () => {
--- module.5/calculator/index.test.js
+++ module.5/calculator/index.test.js
@@ -9,3 +9,27 @@ describe("GET /add", () => {
     expect(res.text).toBe("2 + 3 = 5");
   });
 });
+
+describe("GET /sub", () => {
+  it("subtracts two numbers from query params", async () => {
+    const res = await request(app).get("/sub").query({ a: 2, b: 3 });
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("2 - 3 = -1");
+  });
+});
+
+describe("GET /mul", () => {
+  it("multiplies two numbers from query params", async () => {
+    const res = await request(app).get("/mul").query({ a: 2, b: 3 });
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("2 * 3 = 6");
+  });
+});
+
+describe("GET /div", () => {
+  it("divides two numbers from query params", async () => {
+    const res = await request(app).get("/div").query({ a: 5, b: 2 });
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("5 / 2 = 2.5");
+  });
+});
